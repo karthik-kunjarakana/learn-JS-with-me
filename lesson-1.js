63c368e8e4890a601640d484cb5bc055bc3de53e
karthik-kunjarakana <karthikkunjarakana@gmail.com>
2024-10-02 23:47:14 +0530

    commit1

---
+++ lesson-1.js
@@ -0,0 +1,32 @@
+// Review and Reflect - Lesson 1
+
+// In this lesson, we learned about how to output numbers and text in JavaScript using console.log() method.
+
+// To print numbers, we just add them inside console.log():
+
+console.log(20);
+
+// Output:
+// 20
+
+// Also, we can do mathematical operations inside console method itself, like addition, subtraction etc.
+
+console.log(1 + 2);
+console.log(2 - 2);
+
+// Output:
+// 3
+// 0
+
+// To print text, we either use double quotes or single quotes:
+
+console.log("I am learning JS");
+console.log('JS is fun');
+
+// Output:
+// I am learning JS
+// JS is fun
+
+// You get an error if you don't add double/single quotes when printing text.
+
+// In the next lesson, we will learn about how to print on multiple lines or multiple texts on the same line and do more complex mathematical operations.
